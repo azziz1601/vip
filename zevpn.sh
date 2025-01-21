538e0ea76b8485fcba39153d4590e5006d5e01ce
azziz1601 <aziza924402@gmail.com>
2025-01-21 16:31:12 +0900

    Update zevpn.sh

--- zevpn.sh
+++ zevpn.sh
@@ -33,7 +33,7 @@ echo -e "${YELLOW}----------------------------------------------------------${NC
 echo -e "  Welcome To ZeVPN Tunneling ${YELLOW}(${NC}${green} Stable Edition ${NC}${YELLOW})${NC}"
 echo -e " This Will Quick Setup VPN Server On Your Server"
 echo -e "  Auther : ${green}® ${NC}${YELLOW}(${NC} ${green} ZeVPN Tunneling ${NC}${YELLOW})${NC}"
-echo -e " © Recode By My Self AdijayaStoreVPN Tunneling${YELLOW}(${NC} 2025 ${YELLOW})${NC}"
+echo -e " © Recode By My Self ZEStoreVPN Tunneling${YELLOW}(${NC} 2025 ${YELLOW})${NC}"
 echo -e "${YELLOW}----------------------------------------------------------${NC}"
 echo ""
 sleep 2
