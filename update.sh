be8ad648c579399164a8938a1749354938cdde7d
Diah082 <diahfitriliani9@gmail.com>
2025-03-03 02:48:28 +0700

    Fixx Update

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/aisahblackmarket/vip/main/limit/menu.zip
+    wget https://raw.githubusercontent.com/azziz1601/vip/main/limit/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
